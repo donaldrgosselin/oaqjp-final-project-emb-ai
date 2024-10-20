c4dd554e5b4b5f05c30a6c4b11bd3f5b81458a11
donaldrgosselin <don@dongosselin.com>
2024-10-20 07:49:05 -0400

    Final project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,26 +2,18 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
-    if not text_to_analyse.strip():  # Check for empty or blank input
-        return None
-
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    myobj = {"raw_document": {"text": text_to_analyse}}
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     response = requests.post(url, json=myobj, headers=header)
     formatted_response = json.loads(response.text)
 
     if response.status_code == 200:
-        emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
-        dominant_emotion = max(emotion_predictions.items(), key=lambda x: x[1])[0]
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+    # If the response status code is 500, set label and score to None
+    elif response.status_code == 500:
+        label = None
+        score = None
 
-        return {
-            'anger': emotion_predictions['anger'],
-            'disgust': emotion_predictions['disgust'],
-            'fear': emotion_predictions['fear'],
-            'joy': emotion_predictions['joy'],
-            'sadness': emotion_predictions['sadness'],
-            'dominant_emotion': dominant_emotion
-        }
-    else:
-        return None
+    return {'label': label, 'score': score}
---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detection")
+
+@app.route("/emotionDetector")
+def sent_detector():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    label = response['label']
+    score = response['score']
+    if label is None:
+        return "Invalid input! Try again."
+    else:
+        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+       
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
