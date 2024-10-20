dbe2d39cbc23f78e3fb067364303dd9b2e64eb72
donaldrgosselin <don@dongosselin.com>
2024-10-20 08:10:10 -0400

    Completed project

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
-
 @app.route("/emotionDetector")
+
 def sent_detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
@@ -11,12 +11,11 @@ def sent_detector():
     score = response['score']
     if label is None:
         return "Invalid input! Try again."
-    else:
-        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
-       
+    return "Text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+
 @app.route("/")
+
 def render_index_page():
     return render_template('index.html')
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
